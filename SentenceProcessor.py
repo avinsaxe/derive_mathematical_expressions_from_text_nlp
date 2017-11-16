7047ed1629802797b42230d596deaa5d96040a34
avinsaxe <avinsaxe@tamu.edu>
2017-11-16 14:29:17 -0600

    Initial Framework

--- SentenceProcessor.py
+++ SentenceProcessor.py
@@ -1,22 +1,26 @@
 import sys
 
 class SentenceProcessor:
-    def __init__(self,sentence):
+
+    def __init__(self,sentence,window_size):
         self.sentence=sentence
         self.tags=dict()   #This will store the key as the word in the sentence and the appropriate tag as the value. less_than  is tagged as operator
         self.transformedSentence=None
+        self.window=window_size
+        print "Initialized"
         # This is a list and will store the transformed sentence made entirely of keys from the tags and relative position as in
         # the original sentence
 
     #processes a single sentence
-    def processSentence(self,sentence):
+    def processSentence(self):
         self.operandTagging()
         self.operatorTagging()
 
     #TODO Bhavesh
-    def operatorTagging(self,sentence):
+    def operatorTagging(self):
         print "Start the operator tagging"
 
     #TODO Avinash
-    def operandTagging(self,sentence):
+    def operandTagging(self):
         print "Start the operand tagging"
+
--- starterKit.py
+++ starterKit.py
@@ -1,11 +1,13 @@
 import sys
-import SentenceProcessor
+from SentenceProcessor import SentenceProcessor
 
 
 def main():
     line = raw_input('Enter a sentence:')
-    processor=SentenceProcessor(line)
+    print line
+    processor=SentenceProcessor(line,3)
 
+    processor.processSentence()
 
 if __name__ == "__main__":
     main()
